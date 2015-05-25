a4038541705d6eb9425bda5c944810f4bcb855bc
smurfen <hej@jenserik.se>
2015-05-25 15:37:30 +0200

    ändrar tonart en gång per tryck

--- piano.ino
+++ piano.ino
@@ -19,16 +19,20 @@ int button8state = 0;
 
 const int levelbutton = 12;
 int levelState = 0;
+int levelController = 0;
 
 const int speaker = 13;
 
 int level = 0;
   
-int tones[4][8] = {
+int tones[7][8] = {
 
   { NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1, NOTE_C2 },
+  { NOTE_C2, NOTE_D2, NOTE_E2, NOTE_F2, NOTE_G2, NOTE_A2, NOTE_B2, NOTE_C3 },
+  { NOTE_C3, NOTE_D3, NOTE_E3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_C4 },
   { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5 },
   { NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6 },
+  { NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6, NOTE_C7 },
   { NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7, NOTE_C8 }
 };
 
@@ -59,12 +63,16 @@ void loop() {
   
   levelState = digitalRead(levelbutton);
   
-  if (levelState == HIGH) {
-    if (level >= 4) {
+  if (levelState == HIGH && levelController == 0) {
+    
+    if (level >= 8) {
       level = 0;
     } else {
       level++;
     }
+    levelController = 1;
+  } else if(levelState == LOW) {
+    levelController = 0; 
   }
   
   if(button1state == HIGH)
