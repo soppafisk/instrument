d1d2dd6b76b0e35671900464a6771afd3082c1ca
smurfen <hej@jenserik.se>
2015-05-25 15:16:45 +0200

    tonart funkar typ

--- piano.ino
+++ piano.ino
@@ -17,11 +17,33 @@ int button7state = 0;
 const int button8 = 9;
 int button8state = 0;
 
+const int levelbutton = 12;
+int levelState = 0;
+
 const int speaker = 13;
+
+int level = 0;
   
+int tones[4][8] = {
+
+  { NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1, NOTE_C2 },
+  { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5 },
+  { NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6 },
+  { NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7, NOTE_C8 }
+};
+
 void setup() {
   Serial.begin(9600);
   pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+  pinMode(button5, INPUT);
+  pinMode(button6, INPUT);
+  pinMode(button7, INPUT);
+  pinMode(button8, INPUT);
+  pinMode(levelbutton, INPUT);
+  
   pinMode(speaker, OUTPUT);
 }
 
@@ -35,52 +57,62 @@ void loop() {
   button7state = digitalRead(button7); 
   button8state = digitalRead(button8); 
   
+  levelState = digitalRead(levelbutton);
+  
+  if (levelState == HIGH) {
+    if (level >= 4) {
+      level = 0;
+    } else {
+      level++;
+    }
+  }
+  
   if(button1state == HIGH)
   {
       Serial.println("C");
-      tone(speaker, NOTE_C4, 100);
+      tone(speaker, tones[level][0], 100);
   } 
   
     if(button2state == HIGH)
   {
       Serial.println("D");
-      tone(speaker, NOTE_D4, 100);
+      tone(speaker, tones[level][1], 100);
   } 
   
     if(button3state == HIGH)
   {
-      Serial.println("C");
-      tone(speaker, NOTE_E4, 100);
+      Serial.println("E");
+      tone(speaker, tones[level][2], 100);
   } 
   
     if(button4state == HIGH)
   {
-      Serial.println("släp");
-      tone(speaker, NOTE_F4, 100);
+      Serial.println("F");
+      tone(speaker, tones[level][3], 100);
   } 
   
     if(button5state == HIGH)
   {
-      Serial.println("C");
-      tone(speaker, NOTE_G4, 100);
+      Serial.println("G");
+      tone(speaker, tones[level][4], 100);
   } 
   
     if(button6state == HIGH)
   {
-      Serial.println("C");
-      tone(speaker, NOTE_A4, 100);
+      Serial.println("A");
+      tone(speaker, tones[level][5], 100);
   } 
   
     if(button7state == HIGH)
   {
-      Serial.println("C");
-      tone(speaker, NOTE_B4, 100);
+      Serial.println("B");
+      tone(speaker, tones[level][6], 100);
   } 
   
     if(button8state == HIGH)
   {
       Serial.println("C");
-      tone(speaker, NOTE_C5, 100);
+      tone(speaker, tones[level][7], 100);
   } 
 
 
